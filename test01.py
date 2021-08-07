9a938adaced33410929ceacf74e344981d8d5477
yektakm <ykamaneh@gmail.com>
2021-08-07 20:51:00 +0430

    sth for test

--- test01.py
+++ test01.py
@@ -5,13 +5,27 @@
 # da4752c48d0d4b508ed2421fd409c27a
 
 import os
+import sys
 import spotipy
 import spotipy.util as util
 from spotipy.oauth2 import SpotifyClientCredentials
 
-CLIENT_ID = '9535173dce7c4a5d8049be9aeeb229ba'
-CLIENT_SECRET = 'da4752c48d0d4b508ed2421fd409c27a'
+# Setting the environment variables and getting Spotify's authentication
+os.environ['SPOTIPY_CLIENT_ID'] = '9535173dce7c4a5d8049be9aeeb229ba'
+os.environ['SPOTIPY_CLIENT_SECRET'] = 'da4752c48d0d4b508ed2421fd409c27a'
 
-token = spotipy.SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
-cache_token = token.get_access_token()
-sp = spotipy.Spotify(cache_token)
+spotify = spotipy.Spotify(auth_manager=SpotifyClientCredentials())
+
+
+# The rest is just sth I copied for test
+
+if len(sys.argv) > 1:
+    name = ' '.join(sys.argv[1:])
+else:
+    name = 'Radiohead'
+
+results = spotify.search(q='artist:' + name, type='artist')
+items = results['artists']['items']
+if len(items) > 0:
+    artist = items[0]
+    print(artist['name'], artist['images'][0]['url'])
